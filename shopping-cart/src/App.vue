ebb2e9180abc01dd4758cf40214b00c1a811189d
ollatomiwa <ajaomoshood@gmail.com>
2024-11-03 22:52:54 +0100

    header nav added with directories

--- shopping-cart/src/App.vue
+++ shopping-cart/src/App.vue
@@ -1,13 +1,13 @@
 
 <template>
     <div class="container">
-      <router-view>
-        
-      </router-view>
+      <Header />
+      <router-view></router-view>
     </div>
 </template>
 
   <script setup>
+  import Header from './components/Header.vue';
   
   </script>
 <style scoped>
--- shopping-cart/src/components/ProductItem.vue
+++ shopping-cart/src/components/ProductItem.vue
@@ -1,9 +1,9 @@
 <template>
-
+    
         <div class="col-md-4 mb-2">
-            <div class="card">
+            <div class="card" style="width:'18rem'">
                 <img :src="product.image" class="card-img-top" 
-                height="300" srcset="">
+                height="300" >
                 <div class="card-body">
                     <h5 class="card-title">{{ product.name }}</h5>
                     <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, autem?</p>
