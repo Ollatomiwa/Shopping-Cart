8dc155d0f189f3cb54177a247fe0bfd0bc7c7201
ollatomiwa <ajaomoshood@gmail.com>
2024-11-03 22:18:00 +0100

    products added to display

--- shopping-cart/src/main.js
+++ shopping-cart/src/main.js
@@ -4,12 +4,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Toast from 'vue-toastification'
 import { createPinia } from 'pinia'
+import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap.icons.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.min.js'
 
 createApp(App)
     .use (createPinia())
     .use (Toast)
+    .use(router)
     .mount('#app')
--- shopping-cart/src/stores/useCartStore.js
+++ shopping-cart/src/stores/useCartStore.js
@@ -1,34 +1,36 @@
+import { defineStore } from "pinia"
+
 export const useCartStore = defineStore('cart', {
     state: () => ({ 
 
-    }),
-    getters: {
       products: [
         {
             id: 1,
             name: 'Iphone 15',
             price: 999,
-            image: ''
+            image: 'https://media.istockphoto.com/id/2150443807/photo/two-hands-holding-smart-phone-on-green-background.jpg?s=1024x1024&w=is&k=20&c=P4I5oGXJJYO3JFu6E5PtC6RBj-n4xfx5oNzSfH0WCWs='
         },
         {
             id: 2,
             name: 'Samsung S22',
-            price: 999,
-            image: ''
+            price: 1500,
+            image: 'https://cdn.pixabay.com/photo/2022/03/12/09/23/smartphone-7063762_1280.jpg'
         },
         {
             id: 3,
             name: 'Huawei P',
-            price: 999,
-            image: ''
+            price: 800,
+            image: 'https://cdn.pixabay.com/photo/2020/05/30/23/21/huawei-5240720_640.jpg'
         },
         {
             id: 4,
             name: 'TECNO P15',
             price: 400,
-            image: ''
+            image: 'https://cdn.pixabay.com/photo/2016/03/27/19/43/samsung-1283938_640.jpg'
         },
       ]
+    }),
+    getters: {
     },
     actions: {
       increment() {
