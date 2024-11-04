638b20aad6f01613043ac765d66f522a537cd308
ollatomiwa <ajaomoshood@gmail.com>
2024-11-04 22:12:10 +0100

    cart updated

--- shopping-cart/src/components/Cart.vue
+++ shopping-cart/src/components/Cart.vue
@@ -1,7 +1,43 @@
 <template>
-    cart
+    <div class="row my-4">
+        <div class="col md-12">
+            <div class="card-body">
+                <div class="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                            <th>Subotal</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="items in data.cartItems" :key="items.id">
+                            <td>{{ items.id }}</td>
+                            <td>
+                                <img :src={{ items.Image }}
+                                    width="60"
+                                    height=""
+                                alt="">
+                            </td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
+    import { useCartStore } from '../stores/useCartStore';
 
+    const data = useCartStore();
 </script>
